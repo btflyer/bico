50cc426e45f5814f158bcde1281c44d3955a1793
hadaytullah <hadaytullah@gmail.com>
2019-01-07 18:24:38 +0200

    cb for thermometer

---
+++ v2/cb_thermometer.py
@@ -0,0 +1,21 @@
+from cb_base import CbBase
+
+class CbThermometer(CbBase):
+    """A computational bacterium measuring heat
+    """
+    def __init__(self, env, name, kind, period, context, events):
+
+        super().__init__(self,env,context,events, kind)
+
+        self.name = name
+
+        #Heater properties
+        self.last_temp_read = -1
+
+
+
+    def sustenance_activity(self, context, period):
+        print ("Measuring heat: {} {}".format(self.last_temp_read))
+
+    def on_interrupt_activity():
+        print('Thermometer interrupted')
